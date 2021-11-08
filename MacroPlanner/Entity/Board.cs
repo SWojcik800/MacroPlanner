a91025767b3a8f9afe9de8c5a67d7f5a2c3b9c63
LDVES <szymonwojcik800@gmail.com>
2021-11-08 21:44:48 +0100

    Add EPIC Database Entities

---
+++ MacroPlanner/Entity/Board.cs
@@ -0,0 +1,25 @@
+﻿using MacroPlanner.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MacroPlanner.Entity
+{
+    public class Board
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [MaxLength(70)]
+        [Required]
+        public string Name { get; set; }
+
+
+        public int OwnerId { get; set; }
+        public virtual ApplicationUser Owner { get; set; }
+        public virtual List<Task> Tasks { get; set; }
+
+    }
+}
---
+++ MacroPlanner/Entity/BoardMembers.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MacroPlanner.Entity
+{
+    public enum Role
+    {
+        User,
+        Admin
+    }
+    public class BoardMembers
+    {
+        public int Id { get; set; }
+        public int BoardId { get; set; }
+        public int ApplicationUserId { get; set; }
+        public Role Role { get; set; }
+    }
+}
